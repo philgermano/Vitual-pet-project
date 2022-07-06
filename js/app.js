750546930e5bfc217a0939f5df4e0d314f757ae8
pgermano <philipgermano@gmail.com>
2022-07-05 20:23:54 -0400

    basic layout idea and rough css

---
+++ js/app.js
@@ -0,0 +1,7 @@
+console.log("buffalo");
+////requirements
+
+//game object that will hold game functions that are not directly within the pet and hold relevant player data such as possible score or any modifiers to the world. stuff like background could work as well. 
+
+// a pet class that will be used to make the pet. 
+
