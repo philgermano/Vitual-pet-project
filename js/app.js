979e00b4f8e5d97eef12a5be9d923df0bf3b10c1
pgermano <philipgermano@gmail.com>
2022-07-05 22:21:08 -0400

    can feed pet

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@
 
 // a pet class that will be used to make the pet. 
 
+
 class petBase{
     constructor(name) {
         this.name = name,
@@ -15,24 +16,43 @@ class petBase{
     }
 
     feed(){
-        this.hunger--
+        this.hunger--;
+        let foodMeter = document.querySelector("#hunger");
+        foodMeter.innerText = this.hunger;
     }
     nap(){
         this.sleepiness--
+        let sleepMeter = document.querySelector("#sleepiness");
+        sleepMeter.innerText = this.sleepiness;
     }
     play(){
         this.boredom--
+        let dullMeter = document.querySelector("#boredom");
+        dullMeter.innerText = this.boredom;
     }
 }
 
-const makePet = () =>{
-   let name = prompt("What will you name your pet?", "spot")
+// const makePet = () =>{
+//    let name = prompt("What will you name your pet?", "spot")
 
-    let pet = new petBase(name)     
+//     let pet = new petBase(name)     
 
-    console.log(pet);
-}
+//     console.log(pet);
+// }
 
-const startBut = document.querySelector("#new-pet");
-startBut.addEventListener("click", makePet);
 
+//let name = prompt("What will you name your pet?", "spot")
+
+    let pet = new petBase("whatever")
+
+// const startBut = document.querySelector("#new-pet");
+// startBut.addEventListener("click", makePet);
+
+const feedBut = document.querySelector("#feed");
+feedBut.addEventListener("click", ()=>pet.feed());
+
+const feedBut = document.querySelector("#feed");
+feedBut.addEventListener("click", ()=>pet.feed());
+
+const feedBut = document.querySelector("#feed");
+feedBut.addEventListener("click", ()=>pet.feed());
