3688c6cde3f5b1aa4a224586dd515db3c13ef4b8
pgermano <philipgermano@gmail.com>
2022-07-09 01:03:00 -0400

    cleaned up code, added comments, base stats start at 0, cleaned up readme

--- README.md
+++ README.md
@@ -3,67 +3,15 @@ virtual pet project
 
 Goal create a digital pet.
 
-Requirements.
-4 stats
-	hunger -if maxed = death
-	sleepiness -if maxed = death
-	boredom -fi maxes = death
-	age just increments as time passes
+Pretty much finished.
 
-Buttons for
-	feeding
-	sleep
-	play
-
-Name pet at start
-
-Pet must evolve or morph
-
-Animate pet while it is alive. 
-	can flip image by using css easily.
-	keyframes to shift image around.
-		might be able to use an animated gif set to essentially an idle animation
+Assets used below.
+https://luizmelo.itch.io/fire-worm
+https://rvros.itch.io/pixel-art-animated-slime
 
 
-Possible add ons
+Keep its hunger, sleepiness, and boredom below 10 or it will die. 
 
-Go with a tamagotchi mock up and the buttons on it have words. can use by clocking those options.
-Could rock either flex box or grid.
+Get its high enough and it'll evolve into a second form.
 
-Multiple pet options.
-Pet sounds.
-More activities for pet.
-A shop with toys you can purchase for the pet as presents. - would require a way to get money.
-Animated gif for pet
-
-can use super with class to extend the class to have more properties on it.
-
-So base pet. Then late could add in modifiers or whatever.
-
-TASKS DONE
------------
-pet dies
-pet gains stats and player can reduce them with clicks
-player can name pet
-start button deletes itself and pet image is spawned in
-age displayed and increasing
-need new playzone background image. white box looks bad. needs to darken when pet sleep is hit. set a second mouth that is black and use background blur to blend them for darkness.
-make pet image wander the screen ***keyframes*** Sorta there needs improvement but base done. need to add more options in. set duration and can use a time to know when they finish. like if each pass takes 6 seconds.
-pet evolves
-
-
-TO DO
----------
-
-add detail to tamagatchi image background. stars or something.
-
-
-
-
-CHALLENGES AND ISSUES
----------
-Dont know how to spell(typos)
-figuring out how to turn off setInterval when pet dies so it doesn't keep ticking up the boredom (timer was declared in a function initially so scope issues made me unable to locate it later to stop)
-finding the right set of properties and wording to get the pet to move with keyframe animations.
-set a ton of my css using vh and vw and ended up swapping it all to a set size when i noticed it adjusting poorly to my small laptop screen.
-
+Use the fast or slow buttons to bump the in game speed up or down. 
--- js/app.js
+++ js/app.js
@@ -1,59 +1,35 @@
-//console.log("buffalo");
-////requirements
-
-//game object that will hold game functions that are not directly within the pet and hold relevant player data such as possible score or any modifiers to the world. stuff like background could work as well. 
-
-// a pet class that will be used to make the pet. 
-
-//////TO DO. Make age have a slot on the stat bar. Maybe also pet name. 
-/////Need to spawn in a pet image and delete the make pet button on clicking of the new pet button
-///// find better assets to use. Maybe make myself.
-//////make pet button not centered on mac screen. can do 50% but account for the width of the button to actually make it direct center of playpen.
 let roomDark = false;
 let statTime = "";
 let aniTime = "";
+let ageInc = 0;
 let animationTick = 1000;
     //needed to declare the variable outside of the function so that clearInterval could target it and stop the timer.
-let baseTime = 5000;
+let baseTime = 20000;
+    //arrays of animation frames. time aniTime iterate through array to animate sprite.
 let eggImg = ["art/egg/egg1.png", "art/egg/egg2.png", "art/egg/egg3.png",  "art/egg/egg4.png"]
-
 let slime =["art/pet1/idle/slime-idle-0.png", "art/pet1/idle/slime-idle-1.png", "art/pet1/idle/slime-idle-2.png","art/pet1/idle/slime-idle-3.png"]
-
 let deadSlime =["art/pet1/dead/slime-die-0.png", "art/pet1/dead/slime-die-1.png", "art/pet1/dead/slime-die-2.png", "art/pet1/dead/slime-die-3.png"]
-
 let worm = ["art/pet 2/idle/tile000.png","art/pet 2/idle/tile001.png", "art/pet 2/idle/tile002.png", "art/pet 2/idle/tile003.png", "art/pet 2/idle/tile004.png", "art/pet 2/idle/tile005.png", "art/pet 2/idle/tile006.png", "art/pet 2/idle/tile007.png", "art/pet 2/idle/tile008.png"]
-
 let deadWorm = ["art/pet 2/dead/dead000.png","art/pet 2/dead/dead001.png","art/pet 2/dead/dead002.png","art/pet 2/dead/dead003.png","art/pet 2/dead/dead004.png","art/pet 2/dead/dead005.png","art/pet 2/dead/dead006.png","art/pet 2/dead/dead007.png"]
-
 let wormSleep = ["art/pet 2/sleep/sleep.png"]
 
 let petIn = 0;
 let petState = "egg";
 let sprite = eggImg;
 
-//can change what array sprite points to so everything can be done with one timer
-
-
-//base value declarations above this line
+//base variable declarations above this line
 ///////////////////////////////////////////
-
-// const spawnPet = () =>{
-//     //change img to hatched pet and start new timer for animations and change frames. 
-//     petState = "idle";
-//     let petSpr = document.querySelector("#petSprite");
-//     petSpr.src = "art/pet1/sprite bad.png";
-// }
 
 
 const makePet = () =>{
      //console.log(pet);
          //pet sprite creation 
              alert("Good luck raising your pet. Keep its hunger, sleepiness, and boredom below 10 or it will die. Help it live a long life and who knows what it’ll grow into.\n\n\n Use the fast or slow buttons to bump the in game speed up or down. \n\n Please enjoy yourself.")
-             const petSpr = document.createElement("img");
-             petSpr.setAttribute("id", "petSprite");
-             petSpr.setAttribute("class", "slideright");
-             petSpr.style.width = "100px";
-             petSpr.style.margin = "auto auto";
+                const petSpr = document.createElement("img");
+                    petSpr.setAttribute("id", "petSprite");
+                    petSpr.setAttribute("class", "slideright");
+                        petSpr.style.width = "100px";
+                        petSpr.style.margin = "auto auto";
              ///setting initial state machine
              petState = "egg";
                      //find pet area and put pet in it.
@@ -62,11 +38,17 @@ const makePet = () =>{
                      startBut.remove();
                          animationGo();
  }
- //names pet, starts stat up timer, makes egg img and starts the hatch animation,  
+ //names starts, makes egg img and starts the hatch animation,  
 
  const statUp = () =>{
-
-    pet.age++;
+        //every 2 cycles pet age increases
+    ageInc++;
+        if (ageInc === 2){
+            pet.age++;
+            ageInc = 0;
+        };
+        
+            //boosts 3 states and updates meters with newer values. sets floor of 0 so numbers can't got negative
     pet.boredom++;
         let dullMeter = document.querySelector("#boredom");
         pet.boredom = Math.max(0, pet.boredom);
@@ -83,18 +65,18 @@ const makePet = () =>{
         pet.age =  pet.age;
         ageMeter.innerText =pet.age;    
         
-        if (pet.age >= 20 && petState === "slime"){
+        if (pet.age >= 10 && petState === "slime"){
             alert(`${pet.name} is evolving.`)
             sprite = worm;
             petState = "worm";
             let petSpr = document.querySelector("#petSprite");
         }; 
-        
+                //checks stats and if high enough pet dies.
         if(pet.hunger > 9 || pet.sleepiness > 9 || pet.boredom > 9){
             alert(`${pet.name} is dead.`);
             clearInterval(statTime);
             
-
+                    //checks for current sprite and sets up appropriate death sprites.
             if (sprite === slime){ 
                     petState = "deadSlime";
                     petIn = 0;
@@ -107,14 +89,15 @@ const makePet = () =>{
                     sprite = deadWorm;
                     let petSpr = document.querySelector("#petSprite");
                     petSpr.style.animation = "";
-                    console.log(petState);
-                    console.log(sprite);
+                    //console.log(petState);
+                    //console.log(sprite);
         } 
     }
 }
- 
+        //runs animation. iterates through sprite arrays. covers egg hatching and naming.
 const animationGo = () =>{
     clearInterval(aniTime);
+        //for sprite animation
     aniTime = setInterval(()=>{
         let petSpr = document.querySelector("#petSprite");
         petSpr.src = sprite[petIn];   
@@ -141,8 +124,6 @@ const animationGo = () =>{
                 let petSpr = document.querySelector("#petSprite");
                 petSpr.style.animation = "monster 6s infinite";
                 petSpr.style.animationTimingFunction= "linear";
-                //petSpr.style.animation = "example 4s infinite";
-                //works as example with color change. gotta figure why movement isn't
                 //console.log(petSpr)       
 
                 if(petIn < 3){
@@ -155,6 +136,7 @@ const animationGo = () =>{
             //console.log(sprite);
             //console.log(petSpr);
             //console.log(petState);
+                    //runs through sprite array once if pet is dead. then stays on final death frame.
          } else if (petState === "deadSlime"){
             if(petIn < 3){
                 petIn++
@@ -163,6 +145,7 @@ const animationGo = () =>{
             if(petIn < 7){
                 petIn++
             }
+                    //increases size for worm sprite because it is small than the slime.
         }else if (petState === "worm"){
             let petSpr = document.querySelector("#petSprite");
             petSpr.style.height = "300px";
@@ -184,13 +167,12 @@ const animationGo = () =>{
         //console.log(petSpr);
         //console.log(petState)
      }
-        
+                //variable time to speed up after hatching.
         } , animationTick)
 
 }
-///Boosts up states and checks for if pet dies    
 
-
+///Boosts up stats and checks for if pet dies    
 const timeStart =() =>{
     clearInterval(statTime);
     statTime = setInterval(statUp, (baseTime));
@@ -200,12 +182,13 @@ const timeStart =() =>{
 
 ////////// functions above here
 ////////////////////////
+    //class with base stats meters run off and methods that decreases stats. are triggered on button presses.
 class petBase{
     constructor(name) {
         this.name = name,
-        this.hunger = 5,
-        this.sleepiness = 5,
-        this.boredom = 5,
+        this.hunger = 0,
+        this.sleepiness = 0,
+        this.boredom = 0,
         this.age = 0
     }
 
@@ -234,13 +217,14 @@ const pet = new petBase(null)
 
 
 
-//let name = prompt("What will you name your pet?", "spot")
+////Event Listeners
+////////////////////
 
-    //let pet = new petBase("whatever")
-
+    //Spawns initial pet in and starts everything
  const startBut = document.querySelector("#new-pet");
  startBut.addEventListener("click", makePet);
 
+        //decreases hunger. button changes color for a split second to show it was clicked.
 const feedBut = document.querySelector("#feed");
 feedBut.addEventListener("click", ()=>{
     pet.feed();
@@ -248,12 +232,13 @@ feedBut.addEventListener("click", ()=>{
     setTimeout(()=>{ feedBut.style.backgroundColor = 'aqua'}, 500)
     });
 
+        //decreases sleepiness.
 const napBut = document.querySelector("#nap");
 napBut.addEventListener("click", ()=>{
     pet.nap();
-    napBut.style.backgroundColor = 'grey';
+    napBut.style.backgroundColor = 'grey';       
     setTimeout(()=>{ napBut.style.backgroundColor = 'aqua'}, 500)
-        //DARKENS SCREEN AFTER PRESSING NAP
+        //DARKENS SCREEN AFTER PRESSING NAP sets sprite to a still sleeping one and stops movement.
         if (roomDark === false){
         clearInterval(aniTime);
             if(petState === "worm"){
@@ -266,7 +251,8 @@ napBut.addEventListener("click", ()=>{
         const playPen = document.querySelector("#petZone");
         playPen.setAttribute("id", "petZoneDark")
         let petSpr = document.querySelector("#petSprite");
-            petSpr.style.animation = "";
+        petSpr.style.animation = "";
+            //restarts animation and movement assume pet is not dead.
     setTimeout(()=>{  
         if(petState !== "slimeDead" && petState !== "wormDead"){
             playPen.setAttribute("id", "petZone");
@@ -277,14 +263,14 @@ napBut.addEventListener("click", ()=>{
                 };
             animationGo(); }}, 2000)
     }});
-
+            //decreases boredom
 const playBut = document.querySelector("#play");
-playBut.addEventListener("click", ()=>{
+    playBut.addEventListener("click", ()=>{
     pet.play();
     playBut.style.backgroundColor = 'grey';
     setTimeout(()=>{ playBut.style.backgroundColor = 'aqua'}, 500)
     });
-
+            //speeds up internal clock that time stat increase
 const fastBut = document.querySelector("#fast");
  fastBut.addEventListener("click",() =>{
     clearInterval(statTime);
@@ -293,7 +279,7 @@ const fastBut = document.querySelector("#fast");
     fastBut.style.backgroundColor = 'grey';
     setTimeout(()=>{ fastBut.style.backgroundColor = 'aqua'}, 500)
 });
-
+        //slows down internal clock that controls stat increases.
 const slowBut = document.querySelector("#slow");
  slowBut.addEventListener("click",() =>{clearInterval(statTime);
     statTime = setInterval(statUp, (baseTime *= 2));
@@ -301,16 +287,3 @@ const slowBut = document.querySelector("#slow");
     slowBut.style.backgroundColor = 'grey';
     setTimeout(()=>{ slowBut.style.backgroundColor = 'aqua'}, 500)
 });
-
-
-
-
-// if (buttonRow = "settings"){
-//     let tarBut = document.querySelector("#settings")
-//     tarBut.innerText = settingMenu[4];
-// }
-// if (buttonRow = "main"){
-//     let tarBut = document.querySelector("#settings")
-//     tarBut.innerText = mainMenu[4];
-// }
-//CURRENTLY NOT FUNCTIONING. IGNORE UNTIL WE WANT TO TRY AND GET A SECOND LIST OF MENU OPTIONS UP AND RUNNING,
